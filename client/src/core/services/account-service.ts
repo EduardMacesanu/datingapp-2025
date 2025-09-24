47ba8febb6004d73aa7d6919c807a0f4cd360f31
eduard.macesanu <eduard.macesanu@MAD216I22>
2025-09-24 13:19:31 +0200

    End of section 5

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
-import { User } from '../../types/user';
+import { RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs';
 
 @Injectable({
@@ -12,17 +12,34 @@ export class AccountService {
 
   baseUrl = 'https://localhost:5001/api/';
 
-  login(creds: any) {
-    return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
+  register(creds: RegisterCreds) {
+    return this.http.post<User>(this.baseUrl + 'account/register', creds).pipe(
       tap(user => {
         if (user) {
-          this.currentUser.set(user);
+          this.setCurrentUser(user);
         }
       })
     )
   }
 
+  login(creds: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
+      tap(user => {
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user)); //Almaceno el usuario en el localStorage del navegador
+    this.currentUser.set(user);
+  }
+
+
   logout() {
+    localStorage.removeItem('user'); //Elimino el usuario del localStorage del navegador
     this.currentUser.set(null);
   }
 }
